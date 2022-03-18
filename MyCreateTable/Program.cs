6e9c2a8ae016cbfd7ba945f85ca348a22e18bfb4
Prasetya Agung <prasetya.agung.ft20@mail.umy.ac.id>
2022-03-18 11:31:34 +0700

    Memetakan data

--- MyCreateTable/Program.cs
+++ MyCreateTable/Program.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyCreateTable
+
+namespace CreateTable
 {
     class Program
     {
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source=LAPTOP-C5A9RCLU\\PRASETYAAGUNGP;database=ProdiTI;Integrated Security = TRUE");
+                con.Open();
+                SqlCommand cm = new SqlCommand("create table Mahasiswa_Coba (NIM char(12) not null primary key," +
+                    "Nama varchar(50),Alamat varchar(255), Jenis_Kelamin char(1))", con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Create Table Sukses!");
+                Console.ReadKey();
+            }
+        }
         static void Main(string[] args)
         {
         }
